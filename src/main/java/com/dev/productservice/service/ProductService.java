56a858ac336bd8d8a85b99db391f20f52e17d3c3
Jossany Moura <mjossany@gmail.com>
2023-03-21 21:24:05 -0300

    Create ProductController, ProductRequest and ProductService; Implements createProduct method

---
+++ src/main/java/com/dev/productservice/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.dev.productservice.service;
+
+import com.dev.productservice.dto.ProductRequest;
+import com.dev.productservice.model.Product;
+import com.dev.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+}
