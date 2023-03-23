b23b406c88eb78a2a5eb1fb60d009db18d61b973
Jossany Moura <mjossany@gmail.com>
2023-03-22 23:11:55 -0300

    Create getAllProducts controller and service methods

--- src/main/java/com/dev/productservice/service/ProductService.java
+++ src/main/java/com/dev/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.dev.productservice.service;
 
 import com.dev.productservice.dto.ProductRequest;
+import com.dev.productservice.dto.ProductResponse;
 import com.dev.productservice.model.Product;
 import com.dev.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +26,20 @@ public class ProductService {
 
         productRepository.save(product);
         log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 }
