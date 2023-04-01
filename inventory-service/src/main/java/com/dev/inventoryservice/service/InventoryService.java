bb822ff5948f8efdcb5f2b916459fae3b2cb73af
Jossany Moura <mjossany@gmail.com>
2023-04-01 20:09:21 -0300

    Create Inventory Service first methods

---
+++ inventory-service/src/main/java/com/dev/inventoryservice/service/InventoryService.java
@@ -0,0 +1,18 @@
+package com.dev.inventoryservice.service;
+
+import com.dev.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode().isPresent();
+    }
+}
