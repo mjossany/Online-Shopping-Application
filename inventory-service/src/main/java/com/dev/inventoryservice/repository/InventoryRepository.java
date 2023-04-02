e31f05fb825f575a4df4a0d9645daeda06b9da1c
Jossany Moura <mjossany@gmail.com>
2023-04-01 21:54:25 -0300

    Minor setup configs

--- inventory-service/src/main/java/com/dev/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/dev/inventoryservice/repository/InventoryRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-    Optional<Inventory> findBySkuCode();
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
